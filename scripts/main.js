92d62957db56fc383d2ee8b2aeccd5dcc2864231
Kamila Ignatowicz <kk.ignatowicz@gmail.com>
2016-10-28 20:04:10 +0200

    added clock-outline

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,24 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-	game.load.image('smiley', 'assets/smiley.png');
+
 }
 
 function create() {
-	game.add.sprite(0, 0, 'smiley')
+    game.stage.backgroundColor = "#ffffff";
+
+
+    var graphics = game.add.graphics(0, 0);
+
+
+    graphics.lineStyle(10, 0x000000, 0.8);
+
+    graphics.drawCircle(400, 300, 500);
+    graphics.endFill();
+
+    graphics.beginFill(0x000000);
+    graphics.drawCircle(400,300,10);
+    graphics.endFill();
 }
 
 function update() {
