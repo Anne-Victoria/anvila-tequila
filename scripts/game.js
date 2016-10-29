ed13a4d076bd10f922eb9ee9fdddb0ac227bee14
Kamila Ignatowicz <kk.ignatowicz@gmail.com>
2016-10-29 20:17:43 +0200

    Clock with tile-eating dot

--- scripts/game.js
+++ scripts/game.js
@@ -1,65 +1,180 @@
 var Game = {
 
 	preload : function() {
+		game.load.image('kachel','assets/kachel.png');
 		game.load.image('dot','assets/dot.png');
 		game.load.image('circle','assets/kreis.png');
-		game.load.image('platform','assets/platform.png')
+		game.load.image('platform','assets/platform.png');
 	},
 
 	create : function() {
+
+		//add backgound color
 	    game.stage.backgroundColor = "#ffffff";
 
 	    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-//	    var graphics = game.add.graphics(0, 0);
-		//  The platforms group contains the ground and the 2 ledges we can jump on
+		// create clock-group
     	clock = game.add.group();
 
-//    	graphics.beginFill(0xFF0000, 1);
-//    	graphics.drawCircle(300, 300, 100);
-
-    	//  We will enable physics for any object that is created in this group
+    	// enable physics for any object that is created in this group
     	clock.enableBody = true;
 
+    	// enable collision with world fot any object created in this group
     	clock.collideWorldBounds = true;
 
-//	 	var outline = clock.create(0, 0, 'circle');
-//    	outline.body.immovable = true;
-		
-		circle = new Phaser.Circle(400, 300, 300);
-		circle2 = new Phaser.Circle(400, 300, 30);
+	    // Here we create the ground.
+	    var ground = clock.create(0, game.world.height - 64, 'platform');
 
-		var graphics = game.add.graphics(0, 0);
-    	graphics.lineStyle(3, 0x000000, 1);
-    	graphics.drawCircle(circle.x, circle.y, circle.diameter);
+	    // Scale it to fit the width of the game (the original sprite is 400x32 in size)
+	    ground.scale.setTo(2, 2);
 
-    	graphics.beginFill(0x000000, 1);
-    	graphics.drawCircle(circle2.x, circle2.y, circle2.diameter);
-    	graphics.endFill();
+	    // This stops it from falling away when you jump on it
+	    ground.body.immovable = true;
 
-    	platform = game.add.sprite(400, 300, 'platform');
+	    // create tiles for clock-outline
+	    var tile = clock.create(375, 25, 'kachel');
+	    tile.scale.setTo(0.5, 0.5);
+	    tile.body.immovable = true;
 
-    	game.physics.arcade.enable(platform);
+	    tile = clock.create(125, 275, 'kachel');
+	    tile.scale.setTo(0.5, 0.5);
+	    tile.body.immovable = true;
 
-    	platform.body.collideWorldBounds = true;
+	    tile = clock.create(375, 525, 'kachel');
+	    tile.scale.setTo(0.5, 0.5);
+	    tile.body.immovable = true;
 
-    	platform.scale.setTo(0.5, 0.2);
-    	platform.anchor.setTo(-0.01, 0.5);
+	    tile = clock.create(625, 275, 'kachel');
+	    tile.scale.setTo(0.5, 0.5);
+	    tile.body.immovable = true;
 
-	    player = game.add.sprite(450, 400, 'dot');
+	    x = 325;
+	    y = 25;
+	    for (i = 0; i < 5; i++) { 
+   			tile = clock.create(x, y, 'kachel');
+	   		tile.scale.setTo(0.5, 0.5);
+	    	tile.body.immovable = true;
+	    	x = x - 50;
+	    	y = y + 50;
+		}
 
+		x = 425;
+		y = 25;
+
+		for (i = 0; i < 5; i++) { 
+   			tile = clock.create(x, y, 'kachel');
+	   		tile.scale.setTo(0.5, 0.5);
+	    	tile.body.immovable = true;
+	    	x = x + 50;
+	    	y = y + 50;
+		}
+
+		x = 125;
+		y = 325;
+
+		for (i = 0; i < 5; i++) { 
+   			tile = clock.create(x, y, 'kachel');
+	   		tile.scale.setTo(0.5, 0.5);
+	    	tile.body.immovable = true;
+	    	x = x + 50;
+	    	y = y + 50;
+		}
+
+		x = 425;
+		y = 525;
+
+		for (i = 0; i < 5; i++) { 
+   			tile = clock.create(x, y, 'kachel');
+	   		tile.scale.setTo(0.5, 0.5);
+	    	tile.body.immovable = true;
+	    	x = x + 50;
+	    	y = y - 50;
+		}
+
+		// create group for hande
+		hand = game.add.group();
+
+		//enable physics for every object created in group
+		hand.physicsBodyType = Phaser.Physics.ARCADE;
+    	hand.enableBody = true;
+
+    	//create and align handparts
+	    x = 400;
+		y = 300;
+
+		for (i = 0; i < 13; i++) { 
+   			var handpart = hand.create(x, y, 'kachel');
+	   		handpart.scale.setTo(0.1, 0.1);
+	   		handpart.checkWorldBounds = true;
+	   		handpart.body.velocity.set(100, 100);
+	   		handpart.collideWorldBounds = true;
+	   		handpart.body.bounce.set(1.01);
+	   		handpart.body.gravity.set(0, 180);
+	    	x = x + 10;
+	    	y = y - 10;
+		}
+
+		// add player
+	    player = game.add.sprite(380, 280, 'dot');
+
+	    // enable physics for player
 	    game.physics.arcade.enable(player);
 
 	    player.body.bounce.y = 0.2;
-	    player.body.gravity.y = 300;
+	    player.body.gravity.set(0, 0);
+	    player.checkWorldBounds = true;
 	    player.body.collideWorldBounds = true;
+	    cursors = game.input.keyboard.createCursorKeys();
+
+	    //create onCollide-Signal
+	    player.body.onCollide = new Phaser.Signal();
+
+	    //create function to be colled on collision
+    	player.body.onCollide.add(this.collision, this);
+	},
+
+	// on collision remove handpart
+	collision: function(sprite, handpart) {
+		hand.remove(handpart);
 	},
 
 	update: function() {
-	    game.physics.arcade.collide(player, clock);
-	    game.physics.arcade.collide(player, platform);
 
-	    platform.angle +=1;
+		//declare collisions
+	    game.physics.arcade.collide(player, clock);
+	    game.physics.arcade.collide(player, hand);
+	    game.physics.arcade.collide(clock, hand);
+    
+    	//set player velocity to 0
+    	player.body.velocity.x = 0;
+
+		if (cursors.left.isDown)
+	    {
+	        // Move to the left
+	        player.body.x -= 4;
+	    }
+	    else if (cursors.right.isDown)
+	    {
+	        // Move to the right
+	        player.body.x += 4;
+	    }
+	    else if (cursors.up.isDown)
+	    {
+	    	// Move up
+	    	player.body.y -= 4; 
+	    }
+	    else if (cursors.down.isDown)
+	    {
+	    	// Move down
+	    	player.body.y += 4;
+	    }
+	    else
+	    {
+	        //  Stand still
+	        player.frame = 4;
+	    }
+
 	}
 
 };
