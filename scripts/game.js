56645e95235f6067b1852870e1f6e3cf231f4a1e
Anne-Victoria Meyer <2meyer@informatik.uni-hamburg.de>
2016-10-29 20:15:10 +0200

    added a second player which can be moved on a circular outline

--- scripts/game.js
+++ scripts/game.js
@@ -53,13 +53,46 @@ var Game = {
 	    player.body.bounce.y = 0.2;
 	    player.body.gravity.y = 300;
 	    player.body.collideWorldBounds = true;
+
+	    // starting point for player 2
+	    currentAngle = 0;
+	    var startX = this.calculateX(400, 150, currentAngle);
+	    var startY = this.calculateY(300, 150, currentAngle);
+
+	    // player 2 generation
+	    player2 = game.add.sprite(startX, startY, 'dot');
+	    game.physics.arcade.enable(player2);
+	    player2.anchor.setTo(0.5);
 	},
 
 	update: function() {
 	    game.physics.arcade.collide(player, clock);
 	    game.physics.arcade.collide(player, platform);
 
-	    platform.angle +=1;
+	    // update the position of player 3 on the circle
+	  	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+	  		currentAngle += 0.1;
+	  	}
+	  	if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+	  		currentAngle -= 0.1;
+	  	}
+	  	player2.x = this.calculateX(400, 150, currentAngle);
+	  	player2.y = this.calculateY(300, 150, currentAngle);
+
+	  	// move clock hand
+	  	platform.angle +=1;
+
+	},
+
+	// calculate x and y position on screen for positions on a circle
+	calculateX: function (centerX, radius, angle) {
+		return (centerX + radius * Math.cos(angle));
+	},
+
+	calculateY: function (centerY, radius, angle) {
+		return (centerY + radius * Math.sin(angle));
 	}
+
+
 
 };
